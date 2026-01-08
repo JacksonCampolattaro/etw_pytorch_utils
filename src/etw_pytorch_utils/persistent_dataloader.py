eae82b758acc039afd448a5e705da42ef33d3bd2
Jackson Campolattaro <jackson.campolattaro@gmail.com>
2026-01-08 02:26:05 -0500

    Remove torch._six dependency

--- src/etw_pytorch_utils/persistent_dataloader.py
+++ src/etw_pytorch_utils/persistent_dataloader.py
@@ -14,7 +14,6 @@ import re
 import sys
 import threading
 import traceback
-from torch._six import string_classes, int_classes
 
 
 if sys.version_info[0] == 2:
@@ -144,11 +143,11 @@ def default_collate(batch):
         if elem.shape == ():  # scalars
             py_type = float if elem.dtype.name.startswith("float") else int
             return numpy_type_map[elem.dtype.name](list(map(py_type, batch)))
-    elif isinstance(batch[0], int_classes):
+    elif isinstance(batch[0], int):
         return torch.LongTensor(batch)
     elif isinstance(batch[0], float):
         return torch.DoubleTensor(batch)
-    elif isinstance(batch[0], string_classes):
+    elif isinstance(batch[0], str):
         return batch
     elif isinstance(batch[0], collections.Mapping):
         return {key: default_collate([d[key] for d in batch]) for key in batch[0]}
